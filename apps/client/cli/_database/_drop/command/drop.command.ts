82f01e982a2010180bc671565452e364c0ece8b4
Harivansh <leharivansh@gmail.com>
2024-10-30 22:13:04 +0400

    refactor: use path-alias instead of relative-path

--- apps/client/cli/_database/_drop/command/drop.command.ts
+++ apps/client/cli/_database/_drop/command/drop.command.ts
@@ -2,7 +2,7 @@ import { Inject } from '@nestjs/common';
 import { Db } from 'mongodb';
 import { CommandRunner, SubCommand } from 'nest-commander';
 
-import { DATABASE } from '../../../../../../libs/database/src';
+import { DATABASE } from '@library/database';
 
 @SubCommand({ name: 'drop', description: 'Drop the client database.' })
 export class DropCommand extends CommandRunner {
