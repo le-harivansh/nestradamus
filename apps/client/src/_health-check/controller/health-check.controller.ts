9bd77459c54dd1026e94e344caec33bc791acc58
Harivansh <leharivansh@gmail.com>
2024-11-12 20:04:31 +0400

    feat: implement mongodb health-check

--- apps/client/src/_health-check/controller/health-check.controller.ts
+++ apps/client/src/_health-check/controller/health-check.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { Db } from 'mongodb';
+
+import { DATABASE } from '@library/database';
 
 import { HEALTHCHECK_ROUTE } from '../constant';
+import { MongoDbHealthIndicator } from '../health-indicator/mongo-db.health-indicator';
 
 @Controller(HEALTHCHECK_ROUTE)
 export class HealthCheckController {
-  constructor(private readonly healthCheckService: HealthCheckService) {}
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly mongoDbHealthIndicator: MongoDbHealthIndicator,
+    @Inject(DATABASE) private readonly database: Db,
+  ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.healthCheckService.check([
-      // @todo: Setup health-check indicators.
+      () => this.mongoDbHealthIndicator.canConnectToDatabase(this.database),
+
       // @see: https://docs.nestjs.com/recipes/terminus
     ]);
   }
--- apps/client/src/_health-check/health-check.module.ts
+++ apps/client/src/_health-check/health-check.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 
 import { HealthCheckController } from './controller/health-check.controller';
+import { MongoDbHealthIndicator } from './health-indicator/mongo-db.health-indicator';
 
 @Module({
   imports: [TerminusModule],
   controllers: [HealthCheckController],
+  providers: [MongoDbHealthIndicator],
 })
 export class HealthCheckModule {}
